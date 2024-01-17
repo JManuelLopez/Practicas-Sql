35b764410f8d0f802139449ebe06acb7f02a0f35
unknown <20300545@uttt.edu.mx>
2024-01-17 16:19:02 -0600

    primer commit con ejercicio 1 terminado

---
+++ unidad 1/Entornos db.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE bdEntornosvip
+go
+
+use bdEntornosvip
+go
+
+Create table tblCategoria(
+	categoriaId int not null,
+	descripcion varchar(100) not null,
+	constraint pk_tblCategoria 
+	primary key (categoriaId),
+	constraint unique_descripcion
+	unique (descripcion)
+	)
+go
+
+CREATE TABLE tblProducto(
+  productoId int not null,
+  nombreP varchar(50) not null,
+  precio decimal(10,2) not null,
+  existencia int not null,
+  categoriaId int not null,
+  constraint pk_tblProducto
+  primary key(productoId),
+  constraint unique_nombreP
+  unique(nombreP),
+  constraint chk_precio
+  check(precio>0.0 and precio<=10000),
+  constraint chk_existencia
+  check(existencia>=0),
+  constraint fk_tblProducto_tblcategoria
+  foreign key(categoriaId)
+  references tblcategoria(categoriaId)
+)
+

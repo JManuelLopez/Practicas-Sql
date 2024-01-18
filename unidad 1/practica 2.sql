55189f6e3677a97055006f34dd4bb148b246e1c2
unknown <20300545@uttt.edu.mx>
2024-01-18 16:38:22 -0600

    commit con ejercicio 2 terminado

--- unidad 1/practica 2.sql
+++ unidad 1/practica 2.sql
@@ -23,7 +23,7 @@ nombreEmp varchar(50) not null,
 apellidosEmp varchar(80) not null,
 Sexo char(1) not null,
 constraint chk_sexo
-check (sexo = 'H' or 'M'),
+check (sexo = 'H' or sexo = 'M'),
 salario decimal(10,2)
 constraint chk_salario
 check(salario>400.0 and salario<=50000)
@@ -44,3 +44,32 @@ foreign key(clienteId)
 references tbCliente(clienteId)
 )
 
+CREATE TABLE tbProducto(
+ProductoId int not null,
+constraint pk_productoId
+primary key(ProductoId),
+Pdescripcion varchar(50),
+constraint unique_descripcion
+unique(Pdescripcion),
+Pexistencia int not null,
+PrecioUnitario decimal(10,2)
+constraint chk_PrecioUnit
+check(PrecioUnitario>0.0)
+)
+
+CREATE TABLE tbDetalleVenta (
+VentaId int not null,
+Cantidad int not null,
+Precio decimal (10,2) not null,
+constraint chk_PreciodetailV
+check(Precio>0.0),
+ProductoId int not null,
+constraint pk_DetalleVenta
+primary key (VentaId, ProductoId),
+constraint fk_detalleventa_venta
+foreign key (VentaId)
+references tbVenta(VentaId),
+constraint fk_detalleventa_producto
+foreign key (ProductoId)
+references tbProducto(ProductoId)
+)
